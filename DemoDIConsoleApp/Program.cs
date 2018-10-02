f08a86b1dd6305e76260707da7e3306a23492742
Poy Chang <poypost@gmail.com>
2018-10-02 23:30:00 +0800

    Add project files.

---
+++ DemoDIConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoDIConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
